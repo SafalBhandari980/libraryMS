f461f7402a69a0740488d8fa712737edc7d2c92d
normansingh14-design <normansingh14@gmail.com>
2026-05-22 19:38:36 +0545

    working with book search ui

--- src/view/BookSearch.java
+++ src/view/BookSearch.java
@@ -34,11 +34,11 @@ public class BookSearch extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 933, Short.MAX_VALUE)
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 472, Short.MAX_VALUE)
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
